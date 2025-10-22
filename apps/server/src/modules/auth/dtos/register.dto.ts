c9d9d853eb326cc94d126afcc03ffbb3938def6b
eitan Fixe <allaleitan@gmail.com>
2025-10-22 13:53:37 +0300

    feat: docker is working

--- apps/server/src/modules/auth/dtos/register.dto.ts
+++ apps/server/src/modules/auth/dtos/register.dto.ts
@@ -9,7 +9,4 @@ export class RegisterDto {
   @IsNotEmpty({ message: 'Password is required' })
   @MinLength(6, { message: 'Password must be at least 6 characters long' })
   password!: string;
-
-  @IsString({ message: 'Username must be a string' })
-  username?: string;
 }
